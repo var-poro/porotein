dda5c772394b01c93d8a186b14403a471ba43bf2
var-poro <110412302+var-poro@users.noreply.github.com>
2024-07-07 13:08:16 +0200

    Refactor NumberInput to use useLongPress hook
    This commit substitutes the original callback functions in NumberInput with a new custom hook: useLongPress. This abstraction simplifies the code of the NumberInput component for long press actions, improving its readability and maintainability.

--- api/src/controllers/userController.ts
+++ api/src/controllers/userController.ts
@@ -1,86 +1,87 @@
-import { Request, Response } from 'express';
+import {Request, Response} from 'express';
 import User from '../models/User';
 
 interface AuthRequest extends Request {
-  userId?: string;
+    userId?: string;
 }
 
 export const getCurrentUser = async (req: AuthRequest, res: Response) => {
-  try {
-    const user = await User.findById(req.userId).populate('activeProgram');
-    if (!user) {
-      return res.status(404).send();
+    console.log("Getting current user");
+    try {
+        const user = await User.findById(req.userId).populate('activeProgram');
+        if (!user) {
+            return res.status(404).send();
+        }
+        res.send(user);
+    } catch (error) {
+        res.status(500).send(error);
     }
-    res.send(user);
-  } catch (error) {
-    res.status(500).send(error);
-  }
 };
 
 export const updateCurrentUser = async (req: AuthRequest, res: Response) => {
-  try {
-    const updates = req.body;
-    const user = await User.findByIdAndUpdate(req.userId, updates, { new: true, runValidators: true });
-    if (!user) {
-      return res.status(404).send();
+    try {
+        const updates = req.body;
+        const user = await User.findByIdAndUpdate(req.userId, updates, {new: true, runValidators: true});
+        if (!user) {
+            return res.status(404).send();
+        }
+        res.send(user);
+    } catch (error) {
+        res.status(400).send(error);
     }
-    res.send(user);
-  } catch (error) {
-    res.status(400).send(error);
-  }
 };
 
 export const createUser = async (req: Request, res: Response) => {
-  try {
-    const user = new User(req.body);
-    await user.save();
-    res.status(201).send(user);
-  } catch (error) {
-    res.status(400).send(error);
-  }
+    try {
+        const user = new User(req.body);
+        await user.save();
+        res.status(201).send(user);
+    } catch (error) {
+        res.status(400).send(error);
+    }
 };
 
 export const getUser = async (req: Request, res: Response) => {
-  try {
-    const user = await User.findById(req.params.id);
-    if (!user) {
-      return res.status(404).send();
+    try {
+        const user = await User.findById(req.params.id);
+        if (!user) {
+            return res.status(404).send();
+        }
+        res.send(user);
+    } catch (error) {
+        res.status(500).send(error);
     }
-    res.send(user);
-  } catch (error) {
-    res.status(500).send(error);
-  }
 };
 
 export const updateUser = async (req: Request, res: Response) => {
-  try {
-    const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true });
-    if (!user) {
-      return res.status(404).send();
+    try {
+        const user = await User.findByIdAndUpdate(req.params.id, req.body, {new: true, runValidators: true});
+        if (!user) {
+            return res.status(404).send();
+        }
+        res.send(user);
+    } catch (error) {
+        res.status(400).send(error);
     }
-    res.send(user);
-  } catch (error) {
-    res.status(400).send(error);
-  }
 };
 
 export const deleteUser = async (req: Request, res: Response) => {
-  try {
-    const user = await User.findByIdAndDelete(req.params.id);
-    if (!user) {
-      return res.status(404).send();
+    try {
+        const user = await User.findByIdAndDelete(req.params.id);
+        if (!user) {
+            return res.status(404).send();
+        }
+        res.send(user);
+    } catch (error) {
+        res.status(500).send(error);
     }
-    res.send(user);
-  } catch (error) {
-    res.status(500).send(error);
-  }
 };
 
 export const getAllUsers = async (req: Request, res: Response) => {
-  try {
-    const users = await User.find({});
-    res.send(users);
-  } catch (error) {
-    res.status(500).send(error);
-  }
+    try {
+        const users = await User.find({});
+        res.send(users);
+    } catch (error) {
+        res.status(500).send(error);
+    }
 };
