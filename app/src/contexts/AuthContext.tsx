f6c803b677bca1fe40ca41e5d357b16862263daf
var-poro <poro@variables.fr>
2025-03-20 22:13:54 +0100

    feat(auth): Refactor AuthContext for improved user authentication management
    - Create AuthContext to manage user state and provide login functionality.
    - Implement useAuth hook for easier access to authentication context.
    - Update VerifyMagicLink component to utilize the new AuthContext for user authentication.
    - Enhance type safety by defining AuthContextType interface for better clarity.

--- app/src/contexts/AuthContext.tsx
+++ app/src/contexts/AuthContext.tsx
@@ -1,7 +1,36 @@
-const login = (user: DecodedToken, navigate?: (path: string) => void) => {
-  setUser(user);
-  localStorage.setItem('user', JSON.stringify(user));
-  if (navigate) {
-    navigate('/');
+import { DecodedToken } from '@/utils/tokenUtils';
+import { createContext, useContext, useState } from 'react';
+
+interface AuthContextType {
+  user: DecodedToken | null;
+  setUser: (user: DecodedToken | null) => void;
+  login: (user: DecodedToken, navigate?: (path: string) => void) => void;
+}
+
+const AuthContext = createContext<AuthContextType | undefined>(undefined);
+
+export const AuthProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+  const [user, setUser] = useState<DecodedToken | null>(null);
+
+  const login = (user: DecodedToken, navigate?: (path: string) => void) => {
+    setUser(user);
+    localStorage.setItem('user', JSON.stringify(user));
+    if (navigate) {
+      navigate('/');
+    }
+  };
+
+  return (
+    <AuthContext.Provider value={{ user, setUser, login }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useAuth = () => {
+  const context = useContext(AuthContext);
+  if (context === undefined) {
+    throw new Error('useAuth must be used within an AuthProvider');
   }
+  return context;
 }; 
